4b758659d0c46f82cc4ae29d42e370dd43b25ff6
Winson Chen <wc543@drexel.edu>
2024-02-12 13:39:21 -0500

    Created constructor in Class CommandValidator

--- src/main/java/CommandValidator.java
+++ src/main/java/CommandValidator.java
@@ -1,4 +1,11 @@
 public class CommandValidator {
+
+	private Checking checking;
+
+	public CommandValidator(Checking checking) {
+		this.checking = checking;
+	}
+
 	public boolean validate(String command) {
 		return true;
 	}
--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -5,15 +5,23 @@ import org.junit.jupiter.api.Test;
 
 public class CommandValidatorTest {
 	CommandValidator commandValidator;
+	Checking checking;
 
 	@BeforeEach
 	void setUp() {
-		commandValidator = new CommandValidator();
+		checking = new Checking();
+		commandValidator = new CommandValidator(checking);
 	}
 
 	@Test
 	void create_checking_account() {
 		boolean actual = commandValidator.validate("create checking 12345678 1.2");
+		assertTrue(actual);
+	}
+
+	@Test
+	void create_savings_account() {
+		boolean actual = commandValidator.validate("create savings 22334455 1.2");
 		assertTrue(actual);
 	}
 }
