403ce64e2bb47536d8bdd11ba631e81ce147223e
Winson Chen <wc543@drexel.edu>
2024-02-23 23:33:11 -0500

    Create command Test Cases

--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -1,4 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CommandValidatorTest {
 	CommandValidator commandValidator;
@@ -10,4 +14,21 @@ public class CommandValidatorTest {
 		commandValidator = new CommandValidator(bank);
 	}
 
+	@Test
+	void validate_create_command_is_valid() {
+		boolean actual = commandValidator.validate("create savings 12345678 1.2");
+		assertTrue(actual);
+	}
+
+	@Test
+	void validate_create_command_is_case_insensitive_is_valid() {
+		boolean actual = commandValidator.validate("cReAtE SaViNgs 12345678 1.2");
+		assertTrue(actual);
+	}
+
+	@Test
+	void validate_create_command_is_spelled_wrong_is_invalid() {
+		boolean actual = commandValidator.validate("crate savings 12345678 1.2");
+		assertFalse(actual);
+	}
 }
