d6207ba1e4106f09171f31d7a91ece07c5780fdf
Winson Chen <wc543@drexel.edu>
2024-03-02 00:03:56 -0500

    Refactored MasterControlTest

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MasterControlTest {
+	public static final String INVALID_CREATE_COMMAND = "creat checking 12345678 1.0";
+	public static final String INVALID_DEPOSIT_COMMAND = "depositt 12345678 100";
+	public static final String VALID_CREATE_COMMAND = "create checking 12345678 1.0";
 
 	MasterControl masterControl;
 	List<String> input;
@@ -26,42 +29,42 @@ public class MasterControlTest {
 
 	@Test
 	void typo_in_create_command_is_invalid() {
-		input.add("creat checking 12345678 1.0");
+		input.add(INVALID_CREATE_COMMAND);
 
 		List<String> actual = masterControl.start(input);
 
-		assertSingleCommand("creat checking 12345678 1.0", actual);
+		assertSingleCommand(INVALID_CREATE_COMMAND, actual);
 	}
 
 	@Test
 	void typo_in_deposit_command_is_invalid() {
-		input.add("depositt 12345678 100");
+		input.add(INVALID_DEPOSIT_COMMAND);
 
 		List<String> actual = masterControl.start(input);
 
-		assertSingleCommand("depositt 12345678 100", actual);
+		assertSingleCommand(INVALID_DEPOSIT_COMMAND, actual);
 	}
 
 	@Test
 	void two_typo_commands_both_invalid() {
-		input.add("creat checking 12345678 1.0");
-		input.add("depositt 12345678 100");
+		input.add(INVALID_CREATE_COMMAND);
+		input.add(INVALID_DEPOSIT_COMMAND);
 
 		List<String> actual = masterControl.start(input);
 
 		assertEquals(2, actual.size());
-		assertEquals("creat checking 12345678 1.0", actual.get(0));
-		assertEquals("depositt 12345678 100", actual.get(1));
+		assertEquals(INVALID_CREATE_COMMAND, actual.get(0));
+		assertEquals(INVALID_DEPOSIT_COMMAND, actual.get(1));
 	}
 
 	@Test
 	void invalid_to_create_accounts_with_same_ID() {
-		input.add("create checking 12345678 1.0");
-		input.add("create checking 12345678 1.0");
+		input.add(VALID_CREATE_COMMAND);
+		input.add(VALID_CREATE_COMMAND);
 
 		List<String> actual = masterControl.start(input);
 
-		assertSingleCommand("create checking 12345678 1.0", actual);
+		assertSingleCommand(VALID_CREATE_COMMAND, actual);
 	}
 
 }
