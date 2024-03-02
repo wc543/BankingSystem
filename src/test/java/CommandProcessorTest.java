78928cf109f322492ed47e1e4239882dd081d9e5
Winson Chen <wc543@drexel.edu>
2024-03-01 23:55:53 -0500

    Refactored CommandProcessorTest

--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -4,6 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CommandProcessorTest {
+	public static final String CREATE_CHECKING_ACCOUNT_COMMAND = "create checking 12345678 1.0";
+	public static final String CREATE_SAVINGS_ACCOUNT_COMMAND = "create savings 87654321 1.2";
+	public static final String CREATE_CD_ACCOUNT_COMMAND = "create cd 21436587 1.4 2000";
+	public static final String CHECKING_ACCOUNT_ID = "12345678";
+	public static final String SAVINGS_ACCOUNT_ID = "87654321";
+	public static final String CD_ACCOUNT_ID = "21436587";
+	public static final String DEPOSIT_500_IN_CHECKING_ACCOUNT_COMMAND = "deposit 12345678 500";
+	public static final String DEPOSIT_900_IN_CHECKING_ACCOUNT_COMMAND = "deposit 12345678 900";
+	public static final String DEPOSIT_1300_IN_SAVINGS_ACCOUNT_COMMAND = "deposit 87654321 1300";
+	public static final String DEPOSIT_2000_IN_SAVINGS_ACCOUNT_COMMAND = "deposit 87654321 2000";
+
 	CommandProcessor commandProcessor;
 	Bank bank;
 
@@ -15,101 +26,101 @@ public class CommandProcessorTest {
 
 	@Test
 	void create_checking_account_with_correct_id_and_apr() {
-		commandProcessor.process("create checking 12345678 1.0");
+		commandProcessor.process(CREATE_CHECKING_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("12345678");
+		Account account = bank.getAccount().get(CHECKING_ACCOUNT_ID);
 
 		assertEquals(1.0, account.getApr());
-		assertEquals("12345678", account.getId());
+		assertEquals(CHECKING_ACCOUNT_ID, account.getId());
 	}
 
 	@Test
 	void create_savings_account_with_correct_id_and_apr() {
-		commandProcessor.process("create savings 87654321 1.2");
+		commandProcessor.process(CREATE_SAVINGS_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("87654321");
+		Account account = bank.getAccount().get(SAVINGS_ACCOUNT_ID);
 
 		assertEquals(1.2, account.getApr());
-		assertEquals("87654321", account.getId());
+		assertEquals(SAVINGS_ACCOUNT_ID, account.getId());
 	}
 
 	@Test
 	void create_cd_account_with_correct_id_and_apr() {
-		commandProcessor.process("create cd 21436587 1.4 2000");
+		commandProcessor.process(CREATE_CD_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("21436587");
+		Account account = bank.getAccount().get(CD_ACCOUNT_ID);
 
 		assertEquals(1.4, account.getApr());
-		assertEquals("21436587", account.getId());
+		assertEquals(CD_ACCOUNT_ID, account.getId());
 		assertEquals(2000, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_empty_checking_account() {
-		commandProcessor.process("create checking 12345678 1.0");
-		commandProcessor.process("deposit 12345678 900");
+		commandProcessor.process(CREATE_CHECKING_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_900_IN_CHECKING_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("12345678");
+		Account account = bank.getAccount().get(CHECKING_ACCOUNT_ID);
 
 		assertEquals(900, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_empty_savings_account() {
-		commandProcessor.process("create savings 87654321 1.2");
-		commandProcessor.process("deposit 87654321 1300");
+		commandProcessor.process(CREATE_SAVINGS_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_1300_IN_SAVINGS_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("87654321");
+		Account account = bank.getAccount().get(SAVINGS_ACCOUNT_ID);
 
 		assertEquals(1300, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_checking_account_with_balance() {
-		commandProcessor.process("create checking 12345678 1.0");
-		commandProcessor.process("deposit 12345678 900");
+		commandProcessor.process(CREATE_CHECKING_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_900_IN_CHECKING_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("12345678");
+		Account account = bank.getAccount().get(CHECKING_ACCOUNT_ID);
 
-		commandProcessor.process("deposit 12345678 500");
+		commandProcessor.process(DEPOSIT_500_IN_CHECKING_ACCOUNT_COMMAND);
 
 		assertEquals(1400, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_savings_account_with_balance() {
-		commandProcessor.process("create savings 87654321 1.2");
-		commandProcessor.process("deposit 87654321 1300");
+		commandProcessor.process(CREATE_SAVINGS_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_1300_IN_SAVINGS_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("87654321");
+		Account account = bank.getAccount().get(SAVINGS_ACCOUNT_ID);
 
-		commandProcessor.process("deposit 87654321 2000");
+		commandProcessor.process(DEPOSIT_2000_IN_SAVINGS_ACCOUNT_COMMAND);
 
 		assertEquals(3300, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_checking_account_twice() {
-		commandProcessor.process("create checking 12345678 1.0");
-		commandProcessor.process("deposit 12345678 900");
+		commandProcessor.process(CREATE_CHECKING_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_900_IN_CHECKING_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("12345678");
+		Account account = bank.getAccount().get(CHECKING_ACCOUNT_ID);
 
-		commandProcessor.process("deposit 12345678 500");
-		commandProcessor.process("deposit 12345678 500");
+		commandProcessor.process(DEPOSIT_500_IN_CHECKING_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_500_IN_CHECKING_ACCOUNT_COMMAND);
 
 		assertEquals(1900, account.getBalance());
 	}
 
 	@Test
 	void deposit_into_savings_account_twice() {
-		commandProcessor.process("create savings 87654321 1.2");
-		commandProcessor.process("deposit 87654321 1300");
+		commandProcessor.process(CREATE_SAVINGS_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_1300_IN_SAVINGS_ACCOUNT_COMMAND);
 
-		Account account = bank.getAccount().get("87654321");
+		Account account = bank.getAccount().get(SAVINGS_ACCOUNT_ID);
 
-		commandProcessor.process("deposit 87654321 2000");
-		commandProcessor.process("deposit 87654321 2000");
+		commandProcessor.process(DEPOSIT_2000_IN_SAVINGS_ACCOUNT_COMMAND);
+		commandProcessor.process(DEPOSIT_2000_IN_SAVINGS_ACCOUNT_COMMAND);
 
 		assertEquals(5300, account.getBalance());
 	}
