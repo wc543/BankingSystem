b0aa827ff11adc80e1ba00f47ad6ea4d36c2a342
Winson Chen <wc543@drexel.edu>
2024-03-01 23:44:35 -0500

    Refactored CommandStorageTest

--- src/test/java/CommandStorageTest.java
+++ src/test/java/CommandStorageTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CommandStorageTest {
+	public static final String CREATE_INVALID_CHECKING_ACCOUNT = "crate checking 12345678 1.2";
+	public static final String CREATE_INVALID_SAVINGS_ACCOUNT = "crate savings 87654321 1.4";
+
 	CommandStorage commandStorage;
 	Bank bank;
 
@@ -20,28 +23,28 @@ public class CommandStorageTest {
 
 	@Test
 	void add_one_invalid_command_to_empty_invalid_command_storage() {
-		commandStorage.addInvalidCommand("crate checking 12345678 1.2");
+		commandStorage.addInvalidCommand(CREATE_INVALID_CHECKING_ACCOUNT);
 		assertEquals(1, commandStorage.getInvalidCommands().size());
 	}
 
 	@Test
 	void add_two_invalid_commands_to_empty_invalid_command_storage() {
-		commandStorage.addInvalidCommand("crate checking 12345678 1.2");
-		commandStorage.addInvalidCommand("crate savings 87654321 1.4");
+		commandStorage.addInvalidCommand(CREATE_INVALID_CHECKING_ACCOUNT);
+		commandStorage.addInvalidCommand(CREATE_INVALID_SAVINGS_ACCOUNT);
 		assertEquals(2, commandStorage.getInvalidCommands().size());
 	}
 
 	@Test
 	void check_one_invalid_command_is_added_correctly_to_invalid_command_storage() {
-		commandStorage.addInvalidCommand("crate checking 12345678 1.2");
-		assertEquals("crate checking 12345678 1.2", commandStorage.getInvalidCommands().get(0));
+		commandStorage.addInvalidCommand(CREATE_INVALID_CHECKING_ACCOUNT);
+		assertEquals(CREATE_INVALID_CHECKING_ACCOUNT, commandStorage.getInvalidCommands().get(0));
 	}
 
 	@Test
 	void check_two_invalid_commands_is_added_correctly_to_invalid_command_storage() {
-		commandStorage.addInvalidCommand("crate checking 12345678 1.2");
-		commandStorage.addInvalidCommand("crate savings 87654321 1.4");
-		assertEquals("crate checking 12345678 1.2", commandStorage.getInvalidCommands().get(0));
-		assertEquals("crate savings 87654321 1.4", commandStorage.getInvalidCommands().get(1));
+		commandStorage.addInvalidCommand(CREATE_INVALID_CHECKING_ACCOUNT);
+		commandStorage.addInvalidCommand(CREATE_INVALID_SAVINGS_ACCOUNT);
+		assertEquals(CREATE_INVALID_CHECKING_ACCOUNT, commandStorage.getInvalidCommands().get(0));
+		assertEquals(CREATE_INVALID_SAVINGS_ACCOUNT, commandStorage.getInvalidCommands().get(1));
 	}
 }
