260a4392632a617e60f9fa2493f81afc531a049a
Winson Chen <wc543@drexel.edu>
2024-03-14 14:18:49 -0400

    PassCommandValidatorTest missing arguments Test Cases

---
+++ src/test/java/banking/PassCommandValidatorTest.java
@@ -0,0 +1,35 @@
+package banking;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PassCommandValidatorTest {
+	CommandValidator commandValidator;
+	Bank bank;
+
+	@BeforeEach
+	void setUp() {
+		bank = new Bank();
+		commandValidator = new CommandValidator(bank);
+	}
+
+	@Test
+	void empty_command_is_invalid() {
+		boolean actual = commandValidator.validate("");
+		assertFalse(actual);
+	}
+
+	@Test
+	void command_missing_amount_of_time_is_invalid() {
+		boolean actual = commandValidator.validate("pass");
+		assertFalse(actual);
+	}
+
+	@Test
+	void command_missing_action_is_invalid() {
+		boolean actual = commandValidator.validate("5");
+		assertFalse(actual);
+	}
+}
